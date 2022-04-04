2811ea3d056c99b607f4a7e1aabbbfc0c08ce366
Dato-Zhang <dato12423@163.com>
2022-04-04 23:21:15 +0800

    0404_1.c

---
+++ 0404_1/0404_1/0404_1.c
@@ -0,0 +1,115 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	int x, y = 0;
+//	scanf("%d %d", &x, &y);
+//	int i = 0;
+	//fun2
+	//int min = 0;
+	//min = x > y ? x : y;
+	//for ( ;  ; )
+	//{
+	//	if (min % x == 0 && min % y == 0)
+	//	{
+	//		printf("%d\n", min);
+	//		break;
+	//	}
+	//	min++;
+	//}
+	//fun1
+//	for ( i = 1; ; i++)
+//	{
+//		if (x * i % y == 0)
+//		{
+//			printf("%d\n", x * i);
+//			break;
+//		}
+//		i++;
+//	}
+//	
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int x, y = 0;
+//	int count = 0;
+//	for (x = 0, y = 0; (y = 123) && (x < 4); x++)
+//	{
+//		count++;
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+//int main()
+//{
+//	double x, y = 0;
+//	x = 2;
+//	y = x + 3.0 / 2;
+//	printf("%lf\n", y);
+//	return 0;
+//
+//}
+
+//int a = 1;
+//
+//void test()
+//{
+//	int a = 2;
+//	a += 1;
+//}
+//
+//int main()
+//{
+//	test();
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 10;
+//	int j = 20;
+//	int k = 3;
+//	//k *= i + j;
+//	k = k * (i + j);
+//	printf("%d\n", k);
+//	return 0;
+//}
+// 
+//int cnt = 0;
+//int fib(int n)
+//{
+//	cnt++;
+//	if (n == 0)
+//	{
+//		return 1;
+//	}
+//	else if(n == 1)
+//	{
+//		return 2;
+//	}
+//	else
+//	{
+//		return fib(n - 1) + fib(n - 2);
+//	}
+//}
+//int main()
+//{
+//	fib(12);
+//	printf("%d\n", cnt);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 1;
+//	do
+//	{
+//		printf("%d\n", a);
+//	} while (a--);
+//	return 0;
+//}
